66a28939fd8ed69347929d9b1b952a2fc22b6fe1
Leerv474 <roman.lee474@gmail.com>
2024-09-03 23:59:00 +0300

    quick-git update

--- базы-данных/data-base/static-data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO genres (name)
-VALUES ('Rock'), ('Pop'), ('Classic'), ('Traditional'), ('Metal'), ('Phonk'), ('Electronic');
-
-INSERT INTO audio_types (name)
-VALUES ('Song'), ('Book'), ('Podcast'), ('Melody');
