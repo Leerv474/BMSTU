9f4380cbe9dab856356dad704ab5f2ce7bf782eb
Leerv474 <roman.lee474@gmail.com>
2025-12-26 18:34:09 +0300

    quick-git update

--- blockchain/lab4/deploy.py
+++ blockchain/lab4/deploy.py
@@ -1,14 +1,18 @@
 import json
 from tonsdk.contract.wallet import WalletVersionEnum, Wallets
 from tonsdk.contract.token.nft import NFTCollection, NFTItem
+from tonsdk.boc import Cell
 
+
+item_content_uri = "https://puzzlemania-154aa.kxcdn.com/products/2024/puzzle-schmidt-1000-pieces-random-galaxy.webp"
+nft_item_code_hex = ""
 # --- Load wallet from JSON ---
 def load_wallet_from_json(filename):
     with open(filename) as f:
         data = json.load(f)
     mnemonics = data["mnemonics"]
     _, _, _, wallet = Wallets.from_mnemonics(
-        mnemonics, WalletVersionEnum.v4r2, 0
+        mnemonics, WalletVersionEnum.v3r2, 0
     )
     return wallet
 
@@ -18,7 +22,7 @@ wallet_address = wallet.address.to_string(True, True, True)
 import asyncio
 from tonsdk.utils import to_nano, Address
 from tonsdk.contract.token.nft import NFTCollection, NFTItem
-from pytonlib import TonlibClient
+from pytonlib import TonlibClient, TonlibNoResponse
 import requests
 from pathlib import Path
 
@@ -27,7 +31,7 @@ async def get_client():
     config = requests.get(url).json()
     keystore_dir = '/tmp/ton_keystore'
     Path(keystore_dir).mkdir(parents=True, exist_ok=True)
-    client = TonlibClient(ls_index=2, config=config, keystore=keystore_dir, tonlib_timeout=10)
+    client = TonlibClient(ls_index=7, config=config, keystore=keystore_dir, tonlib_timeout=10)
     await client.init()
     return client
 
@@ -35,12 +39,27 @@ async def get_seqno(client: TonlibClient, address: str):
     data = await client.raw_run_method(method='seqno', stack_data=[], address=address)
     return int(data['stack'][0][1], 16)
 
+
+async def get_seqno_with_retry(client, address, retries=3):
+    for retry in range(retries):
+        try:
+            return await get_seqno(client, address)
+        except TonlibNoResponse:
+            print("attempt %d failed\ntrying again...", retry)
+            await asyncio.sleep(5)
+    raise RuntimeError("Failed to get seqno after retries")
+
 def create_collection():
     royalty_base = 1000
     royalty_factor = 55
-    NFT_ITEM_CODE_HEX = "b5ee9c7201010401003e000114ff00f4a413f4bcf2c80b0102016202030202cc04050015a70f605da89a1f401f481f481a9a30201ce06070201280a0b02f70831c02497c138007434c0c05c6c2544d7c0fc07783e903e900c7e800c5c75c87e800c7e800c1cea6d0000b4c7c076cf16cc8d0d0d09208403e29fa96ea68c1b088d978c4408fc06b809208405e351466ea6cc1b08978c840910c03c06f80dd6cda0841657c1ef2ea7c09c6c3cb4b01408eebcb8b1807c073817c160080900113e910c30003cb85360005c804ff833206e953080b1f833de206ef2d29ad0d30731d3ffd3fff404d307d430d0fa00fa00"
-    assert all(c in '0123456789abcdefABCDEF' for c in NFT_ITEM_CODE_HEX), "Невалидный hex в коде NFT Item!"
-    print("Код валиден, длина:", len(NFT_ITEM_CODE_HEX))
+
+    from pathlib import Path
+
+    boc_bytes = Path("nft-item-output.boc").read_bytes()
+    nft_item_code_hex = boc_bytes.hex()
+    assert len(nft_item_code_hex) % 2 == 0
+    assert all(c in '0123456789abcdefABCDEF' for c in nft_item_code_hex), "Невалидный hex в коде NFT Item!"
+    print("Код валиден, длина:", len(nft_item_code_hex))
     collection = NFTCollection(
         royalty_base=royalty_base,
         royalty_factor=royalty_factor,
@@ -48,23 +67,49 @@ def create_collection():
         owner_address=Address(wallet_address),
         collection_content_uri='https://s.getgems.io/nft/b/c/62fba50217c3fe3cbaad9e7f/meta.json',
         nft_item_content_base_uri='https://s.getgems.io/nft/b/c/62fba50217c3fe3cbaad9e7f/',
-        nft_item_code_hex=NFT_ITEM_CODE_HEX
+        nft_item_code_hex=nft_item_code_hex
     )
     return collection
 
 async def deploy_collection(collection):
+
+    code = collection.options['nft_item_code_hex']
+    print(type(code))
+    print(len(code))
+    print(code[:100])
     client = await get_client()
     state_init = collection.create_state_init()['state_init']
-    seqno = await get_seqno(client, wallet_address)
+    seqno = await get_seqno_with_retry(client, wallet_address, 10)
     query = wallet.create_transfer_message(
         to_addr=collection.address.to_string(),
         amount=to_nano(0.05, 'ton'),  # Комиссия для деплоя
         seqno=seqno,
         state_init=state_init
     )
+
+    data = await client.raw_run_method(method='get_account_state', stack_data=[], address=wallet_address)
+    print(data)
     await client.raw_send_message(query['message'].to_boc(False))
     print(f"Адрес коллекции: {collection.address.to_string(True, True, True)}")
+
+async def create_mint(collection, item_content_uri):
+    from pathlib import Path
+
+    payload_boc = Path("nft-mint.boc").read_bytes()
+    client = await get_client()
+    seqno = await get_seqno_with_retry(client, wallet_address, 10)
+
+    msg = wallet.create_transfer_message(
+        to_addr=collection.address.to_string(),
+        amount=to_nano(0.01, 'ton'),  # gas for mint
+        seqno=seqno,
+        payload=payload_boc
+    )
+
+    await client.raw_send_message(msg['message'].to_boc(False))
+
 
 if __name__ == '__main__':
     collection = create_collection()
     asyncio.run(deploy_collection(collection))
+    asyncio.run(create_mint(collection, item_content_uri))
